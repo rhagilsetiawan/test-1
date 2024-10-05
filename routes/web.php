84a14488cb7c0d90bad7164c9c88263b06762189
rhagilsetiawan <159235607+rhagilsetiawan@users.noreply.github.com>
2024-10-06 03:30:27 +0700

    coba auth

--- routes/web.php
+++ routes/web.php
@@ -8,27 +8,27 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Auth::routes();
+Auth::routes();
 
 // Route::get('/products/data-big', [DataController::class, 'productsBig'])->name('products.databig'); // DataTableBig
 // Route::get('/products/data-small', [DataController::class, 'productsSmall'])->name('products.datasmall'); // DataTableSmall
 Route::get('/products/api/{id?}', [DataController::class, 'index'])->name('products.api'); // dashboard
 
-Route::get('/', [HomeController::class, 'index'])->name('dashboard');
-Route::resource('products', ProductController::class);
-Route::resource('shops', ShopController::class);
-Route::resource('transactions', TransactionController::class);
+// Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+// Route::resource('products', ProductController::class);
+// Route::resource('shops', ShopController::class);
+// Route::resource('transactions', TransactionController::class);
 
 // Route::get('/jarak', [HomeController::class, 'jarak'])->name('jarak');
 // Route::resource('products', PlaceController::class);
 // Route::resource('shops', PlaceController::class);
 // Route::resource('transactions', PlaceController::class);
 
-// Route::group(['middleware' => ['auth']], function () {
-//     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
-//     // Route::get('/jarak', [mapController::class, 'jarak'])->name('jarak');
-//     Route::resource('products', ProductController::class);
-//     Route::resource('shops', ShopController::class);
-//     Route::resource('transactions', TransactionController::class);
-// });
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+    // Route::get('/jarak', [mapController::class, 'jarak'])->name('jarak');
+    Route::resource('products', ProductController::class);
+    Route::resource('shops', ShopController::class);
+    Route::resource('transactions', TransactionController::class);
+});
 
