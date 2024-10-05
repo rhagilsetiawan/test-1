124c23d05c55d7194d382fd819e160770791e4a2
rhagilsetiawan <159235607+rhagilsetiawan@users.noreply.github.com>
2024-10-06 02:52:37 +0700

    fix it

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
+use App\Http\Controllers\Api\DataController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/products/data-big', [DataController::class, 'productsBig'])->name('products.databig'); // DataTableBig
+// Route::get('/products/data-small', [DataController::class, 'productsSmall'])->name('products.datasmall'); // DataTableSmall
+Route::get('/products/api/{id?}', [DataController::class, 'index'])->name('products.api'); // dashboard
+
+Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+Route::resource('products', ProductController::class);
+Route::resource('shops', ShopController::class);
+Route::resource('transactions', TransactionController::class);
+
+// Route::get('/jarak', [HomeController::class, 'jarak'])->name('jarak');
+// Route::resource('products', PlaceController::class);
+// Route::resource('shops', PlaceController::class);
+// Route::resource('transactions', PlaceController::class);
+
+// Route::group(['middleware' => ['auth']], function () {
+//     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+//     // Route::get('/jarak', [mapController::class, 'jarak'])->name('jarak');
+//     Route::resource('products', ProductController::class);
+//     Route::resource('shops', ShopController::class);
+//     Route::resource('transactions', TransactionController::class);
+// });
+
